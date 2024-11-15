28fffbbccd8d8d56a04531e0e9379c6d8ca4be83
Brunoburgoa <brunocsir515@gmail.com>
2024-11-15 05:06:17 -0300

    ultima cosa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./containets/pages/Home";
 import Error404 from "./containets/errors/Error404";
 import { UserProvider } from "./context/UserContext";
 import AuthRoutes from "./routes/AuthRoutes"; // Importa las rutas de autenticación
 import DireccionesPage from "./components/navigation/DireccionesPage";
-import ModificarCosto from "./containets/pages/dashboard/contable/ModificarCosto"
-import RecargarStock from "./containets/pages/dashboard/contable/RecargarStock"
 const App = () => {
   return (
     <BrowserRouter>
@@ -15,8 +14,6 @@ const App = () => {
           <Route path="/auth/*" element={<AuthRoutes />} /> {/* Rutas de autenticación */}
           <Route path="/direcciones" element={<DireccionesPage />} />
           <Route path="*" element={<Error404 />} />
-          <Route path="/modificarCostos" element={<ModificarCosto />} />
-          <Route path="/recargarStock" element={<RecargarStock />} />
           
         </Routes>
       </UserProvider>
